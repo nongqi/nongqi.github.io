bb1013e3783cfdd76b5a9e74f378d0fea56282fa
nongqi <771440224@qq.com>
2022-11-22 05:51:04 +0800

    feat: 用 fs 保存文件

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 /*
  * @Author: nongqi
  * @Date: 2022-11-19 01:11:02
- * @LastEditTime: 2022-11-22 02:13:06
+ * @LastEditTime: 2022-11-22 05:50:20
  * @LastEditors: nongqi
  * @Description: index
  */
-import { readFileSync } from 'fs';
+import { existsSync, mkdirSync, readFileSync, writeFileSync } from 'fs';
 import { dirname, resolve } from 'path';
 import { fileURLToPath } from 'url';
+import dayjs from 'dayjs';
 import { generate } from './lib/generator.js';
 import { createRandomPicker } from './lib/random.js';
 
@@ -21,10 +22,25 @@ function loadCorpus(src) {
   return JSON.parse(data);
 }
 
+function saveCorpus(title, article) {
+  const outputDir = resolve(__dirname, 'output');
+  const time = dayjs().format('_YYYY-MM-DD_HH-mm-ss');
+  const outputFile = resolve(outputDir, `${title}${time}.txt`);
+
+  if (!existsSync(outputDir)) {
+    mkdirSync(outputDir);
+  }
+
+  const text = `${title}\n\n    ${article.join('\n    ')}`;
+  writeFileSync(outputFile, text);
+
+  return outputFile;
+}
+
 const corpus = loadCorpus('corpus/data.json');
 const pickTitle = createRandomPicker(corpus.title);
 const title = pickTitle();
 
 const article = generate(title, { corpus });
 
-console.log(`${title}\n\n    ${article.join('\n    ')}`);
+saveCorpus(title, article);
