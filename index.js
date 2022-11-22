a2f50a5166e00a2ec44645777d63433aa0414998
nongqi <771440224@qq.com>
2022-11-22 10:58:09 +0800

    feat: 检查用户输入

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: nongqi
  * @Date: 2022-11-19 01:11:02
- * @LastEditTime: 2022-11-22 10:27:41
+ * @LastEditTime: 2022-11-22 10:56:38
  * @LastEditors: nongqi
  * @Description: index
  */
@@ -9,10 +9,40 @@ import { existsSync, mkdirSync, readFileSync, writeFileSync } from 'fs';
 import { dirname, resolve } from 'path';
 import { fileURLToPath } from 'url';
 import dayjs from 'dayjs';
+import commandLineArgs from 'command-line-args';
+import commandLineUsage from 'command-line-usage';
 import { generate } from './lib/generator.js';
 import { createRandomPicker } from './lib/random.js';
 
 
+// 定义帮助的内容
+const sections = [
+  {
+    header: '狗屁不通文章生成器',
+    content: '生成随机的文章段落用于测试',
+  },
+  {
+    header: 'Options',
+    optionList: [
+      {
+        name: 'title',
+        typeLabel: '{underline string}',
+        description: '文章的主题。',
+      },
+      {
+        name: 'min',
+        typeLabel: '{underline number}',
+        description: '文章最小字数。',
+      },
+      {
+        name: 'max',
+        typeLabel: '{underline number}',
+        description: '文章最大字数。',
+      },
+    ],
+  },
+];
+const usage = commandLineUsage(sections); // 生成帮助文本
 // 获取当前脚本文件所在目录
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -37,30 +67,23 @@ function saveCorpus(title, article) {
   return outputFile;
 }
 
-function parseOptions(options = {}) {
-  const argv = process.argv;
-
-  for (let i = 2; i < argv.length; i++) {
-    const cmd = argv[i - 1];
-    const value = argv[i];
-    if (cmd === '--title') {
-      options.title = value;
-    } else if (cmd === '--min') {
-      options.min = Number(value);
-    } else if (cmd === '--max') {
-      options.max = Number(value);
-    }
-  }
-
-  return options;
-}
-
 const corpus = loadCorpus('corpus/data.json');
-const options = parseOptions();
-const title = options.title || createRandomPicker(corpus.title)();
 
-const article = generate(title, { corpus, ...options });
+const optionDefinitions = [
+  { name: 'help' }, // help命令配置
+  { name: 'title', type: String },
+  { name: 'min', type: Number },
+  { name: 'max', type: Number },
+];
+const options = commandLineArgs(optionDefinitions);
+if ('help' in options) {
+  console.log(usage);
+} else {
+  const title = options.title || createRandomPicker(corpus.title)();
 
-const output = saveCorpus(title, article);
+  const article = generate(title, { corpus, ...options });
 
-console.log(`生成成功！文章保存于：${output}`);
+  const output = saveCorpus(title, article);
+
+  console.log(`生成成功！文章保存于：${output}`);
+}
